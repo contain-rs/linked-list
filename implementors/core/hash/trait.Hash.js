6768de9b01940ea07d6f2ec2a5b1cab289cf7570
FlashCat <FlashCat@users.noreply.github.com>
2015-04-17 15:42:20 +0000

    rebuild pages at ae41d57

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['linked_list'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;A: <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='linked_list/struct.LinkedList.html' title='linked_list::LinkedList'>LinkedList</a>&lt;A&gt;",];
+implementors['linked_list'] = ["impl&lt;A: <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='linked_list/struct.LinkedList.html' title='linked_list::LinkedList'>LinkedList</a>&lt;A&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/iter/trait.Extend.js
+++ implementors/core/iter/trait.Extend.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['linked_list'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;A&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Extend.html' title='core::iter::Extend'>Extend</a>&lt;A&gt; for <a class='struct' href='linked_list/struct.LinkedList.html' title='linked_list::LinkedList'>LinkedList</a>&lt;A&gt;",];
+implementors['linked_list'] = ["impl&lt;A&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Extend.html' title='core::iter::Extend'>Extend</a>&lt;A&gt; for <a class='struct' href='linked_list/struct.LinkedList.html' title='linked_list::LinkedList'>LinkedList</a>&lt;A&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
